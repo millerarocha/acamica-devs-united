a64a39473e6aee082c97033175915513bf81d71a
MILLER ANDRES ROCHA CASTAÑO <millerarocha@gmail.com>
2021-12-06 14:39:57 -0500

    feat:colorPicker color selection functionality

--- src/containers/ColorPicker/ColorPicker.jsx
+++ src/containers/ColorPicker/ColorPicker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 
 import './ColorPicker.scss';
 
@@ -9,14 +9,27 @@ import {colors} from '../../utils/colors';
 const ColorPicker = ({
     title='Select your favorite color'
 }) => {
+
+    const [colorsList,setColorsList] = useState(colors);
+
+    const updateColorChecked = (value) =>{
+        const elementIndex = colorsList.findIndex(element => element.name === value);
+        let newArray = [...colorsList];        
+        newArray.forEach(element=>element.checked = false);
+        newArray[elementIndex] = {...newArray[elementIndex], checked: true}
+        setColorsList(newArray);
+    }
+
     return (
         <div className='color-picker'>
             <h3 className="color-picker__title">{title}</h3>
             <div className="color-picker__container">
-                {(colors || []).map(color=>
+                {(colorsList || []).map(color=>
                     <ColorButton
+                        key={color.id}
                         color={color.name}
                         selected={color.checked}
+                        onClick={()=>{updateColorChecked(color.name)}}
                     />    
                 )}
                 
